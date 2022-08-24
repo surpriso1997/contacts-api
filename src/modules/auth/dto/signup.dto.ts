3296274d7e13564a2934fa93c755922d13fc2cf7
mcgain <t@t.com>
2022-08-24 16:14:56 +0545

    Added swagger configuration in the dto and controllers

--- src/modules/auth/dto/signup.dto.ts
+++ src/modules/auth/dto/signup.dto.ts
@@ -1,13 +1,16 @@
 import { IsEmail, IsPhoneNumber, IsString, MinLength } from 'class-validator';
-
+import { ApiProperty } from '@nestjs/swagger';
 export class SignUpDto {
+  @ApiProperty()
   @IsEmail()
   email: string;
 
+  @ApiProperty()
   @MinLength(6)
   @IsString()
   password: string;
 
+  @ApiProperty()
   @IsPhoneNumber()
   phoneNumber: string;
 }
--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,7 +1,8 @@
 import { Controller, Get, Body, Patch, Param, Delete } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dto/update-user.dto';
-
+import { ApiTags } from '@nestjs/swagger';
+@ApiTags('Users management')
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
